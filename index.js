20ced9c6c64f5752ed2e7f934af68f11dd38d7c7
Jeffery Phipps <jefferyphipps@att.net>
2024-08-12 11:40:03 -0500

    displaying cards right now. Games to be added next

---
+++ index.js
@@ -0,0 +1,403 @@
+const deck = [{
+    suit: "spades",
+    type: "ace",
+    value: 1,
+    image: "./cards/ace_of_spades.png",
+    visable: true
+}, 
+{
+    suit: "spades",
+    type: "two",
+    value: 2,
+    image: "./cards/2_of_spades.png",
+    visable: true
+},
+{
+    suit: "spades",
+    type: "three",
+    value: 3,
+    image: "./cards/3_of_spades.png",
+    visable: true
+},
+{
+    suit: "spades",
+    type: "four",
+    value: 4,
+    image: "./cards/4_of_spades.png",
+    visable: true
+},
+{
+    suit: "spades",
+    type: "five",
+    value: 5,
+    image: "./cards/5_of_spades.png",
+    visable: true
+},
+{
+    suit: "spades",
+    type: "six",
+    value: 6,
+    image: "./cards/6_of_spades.png",
+    visable: true
+},
+{
+    suit: "spades",
+    type: "seven",
+    value: 7,
+    image: "./cards/7_of_spades.png",
+    visable: true
+},
+{
+    suit: "spades",
+    type: "eight",
+    value: 8,
+    image: "./cards/8_of_spades.png",
+    visable: true
+},
+{
+    suit: "spades",
+    type: "nine",
+    value: 9,
+    image: "./cards/9_of_spades.png",
+    visable: true
+},
+{
+    suit: "spades",
+    type: "10",
+    value: 10,
+    image: "./cards/10_of_spades.png",
+    visable: true
+},
+{
+    suit: "spades",
+    type: "jack",
+    value: 10,
+    image: "./cards/jack_of_spades.png",
+    visable: true
+},
+{
+    suit: "spades",
+    type: "queen",
+    value: 11,
+    image: "./cards/queen_of_spades.png",
+    visable: true
+},
+{
+    suit: "spades",
+    type: "king",
+    value: 12,
+    image: "./cards/king_of_spades.png",
+    visable: true
+},
+{
+    suit: "hearts",
+    type: "ace",
+    value: 1,
+    image: "./cards/ace_of_hearts.png",
+    visable: true
+}, 
+{
+    suit: "hearts",
+    type: "two",
+    value: 2,
+    image: "./cards/2_of_hearts.png",
+    visable: true
+},
+{
+    suit: "hearts",
+    type: "three",
+    value: 3,
+    image: "./cards/3_of_hearts.png",
+    visable: true
+},
+{
+    suit: "hearts",
+    type: "four",
+    value: 4,
+    image: "./cards/4_of_hearts.png",
+    visable: true
+},
+{
+    suit: "hearts",
+    type: "five",
+    value: 5,
+    image: "./cards/5_of_hearts.png",
+    visable: true
+},
+{
+    suit: "hearts",
+    type: "six",
+    value: 6,
+    image: "./cards/6_of_hearts.png",
+    visable: true
+},
+{
+    suit: "hearts",
+    type: "seven",
+    value: 7,
+    image: "./cards/7_of_hearts.png",
+    visable: true
+},
+{
+    suit: "hearts",
+    type: "eight",
+    value: 8,
+    image: "./cards/8_of_hearts.png",
+    visable: true
+},
+{
+    suit: "hearts",
+    type: "nine",
+    value: 9,
+    image: "./cards/9_of_hearts.png",
+    visable: true
+},
+{
+    suit: "hearts",
+    type: "10",
+    value: 10,
+    image: "./cards/10_of_hearts.png",
+    visable: true
+},
+{
+    suit: "hearts",
+    type: "jack",
+    value: 10,
+    image: "./cards/jack_of_hearts.png",
+    visable: true
+},
+{
+    suit: "hearts",
+    type: "queen",
+    value: 11,
+    image: "./cards/queen_of_hearts.png",
+    visable: true
+},
+{
+    suit: "hearts",
+    type: "king",
+    value: 12,
+    image: "./cards/king_of_hearts.png",
+    visable: true
+},
+{
+    suit: "diamonds",
+    type: "ace",
+    value: 1,
+    image: "./cards/ace_of_diamonds.png",
+    visable: true
+}, 
+{
+    suit: "diamonds",
+    type: "two",
+    value: 2,
+    image: "./cards/2_of_diamonds.png",
+    visable: true
+},
+{
+    suit: "diamonds",
+    type: "three",
+    value: 3,
+    image: "./cards/3_of_diamonds.png",
+    visable: true
+},
+{
+    suit: "diamonds",
+    type: "four",
+    value: 4,
+    image: "./cards/4_of_diamonds.png",
+    visable: true
+},
+{
+    suit: "diamonds",
+    type: "five",
+    value: 5,
+    image: "./cards/5_of_diamonds.png",
+    visable: true
+},
+{
+    suit: "diamonds",
+    type: "six",
+    value: 6,
+    image: "./cards/6_of_diamonds.png",
+    visable: true
+},
+{
+    suit: "diamonds",
+    type: "seven",
+    value: 7,
+    image: "./cards/7_of_diamonds.png",
+    visable: true
+},
+{
+    suit: "diamonds",
+    type: "eight",
+    value: 8,
+    image: "./cards/8_of_diamonds.png",
+    visable: true
+},
+{
+    suit: "diamonds",
+    type: "nine",
+    value: 9,
+    image: "./cards/9_of_diamonds.png",
+    visable: true
+},
+{
+    suit: "diamonds",
+    type: "10",
+    value: 10,
+    image: "./cards/10_of_diamonds.png",
+    visable: true
+},
+{
+    suit: "diamonds",
+    type: "jack",
+    value: 10,
+    image: "./cards/jack_of_diamonds.png",
+    visable: true
+},
+{
+    suit: "diamonds",
+    type: "queen",
+    value: 11,
+    image: "./cards/queen_of_diamonds.png",
+    visable: true
+},
+{
+    suit: "diamonds",
+    type: "king",
+    value: 12,
+    image: "./cards/king_of_diamonds.png",
+    visable: true
+},
+{
+    suit: "clubs",
+    type: "ace",
+    value: 1,
+    image: "./cards/ace_of_clubs.png",
+    visable: true
+}, 
+{
+    suit: "clubs",
+    type: "two",
+    value: 2,
+    image: "./cards/2_of_clubs.png",
+    visable: true
+},
+{
+    suit: "clubs",
+    type: "three",
+    value: 3,
+    image: "./cards/3_of_clubs.png",
+    visable: true
+},
+{
+    suit: "clubs",
+    type: "four",
+    value: 4,
+    image: "./cards/4_of_clubs.png",
+    visable: true
+},
+{
+    suit: "clubs",
+    type: "five",
+    value: 5,
+    image: "./cards/5_of_clubs.png",
+    visable: true
+},
+{
+    suit: "clubs",
+    type: "six",
+    value: 6,
+    image: "./cards/6_of_clubs.png",
+    visable: true
+},
+{
+    suit: "clubs",
+    type: "seven",
+    value: 7,
+    image: "./cards/7_of_clubs.png",
+    visable: true
+},
+{
+    suit: "clubs",
+    type: "eight",
+    value: 8,
+    image: "./cards/8_of_clubs.png",
+    visable: true
+},
+{
+    suit: "clubs",
+    type: "nine",
+    value: 9,
+    image: "./cards/9_of_clubs.png",
+    visable: true
+},
+{
+    suit: "clubs",
+    type: "10",
+    value: 10,
+    image: "./cards/10_of_clubs.png",
+    visable: true
+},
+{
+    suit: "clubs",
+    type: "jack",
+    value: 10,
+    image: "./cards/jack_of_clubs.png",
+    visable: true
+},
+{
+    suit: "clubs",
+    type: "queen",
+    value: 11,
+    image: "./cards/queen_of_clubs.png",
+    visable: true
+},
+{
+    suit: "clubs",
+    type: "king",
+    value: 12,
+    image: "./cards/king_of_clubs.png",
+    visable: true
+}
+];
+
+window.addEventListener("load", function() {
+    const disDeck = document.getElementById("displayDeck");
+    //displayCard(disDeck, deck);
+    let shuffled = shuffle(deck);
+    //displayCard(disDeck,shuffled);
+    
+});
+
+
+
+function displayCard(disDeck, deck){
+    let display = disDeck;
+    for(let x =0; x<deck.length; x++){
+     let img = document.createElement("img");
+        img.src = deck[x].image;
+        display.appendChild(img);
+
+
+    }
+}
+
+function shuffle(deck){
+    let tempDeck = deck;
+    for (let x = 0; x<365; x++){
+        let random1 = Math.floor(Math.random()*deck.length);
+        let random2 = Math.floor(Math.random()*deck.length);
+        let temp;
+        
+        temp = tempDeck[random1];
+        tempDeck[random1] = tempDeck[random2];
+        tempDeck[random2] = temp;
+
+    }
+    return tempDeck;
+    
+
+}
